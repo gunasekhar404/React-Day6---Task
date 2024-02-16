a36c6cddd5150bc5a159f87eeeb0b65e5b19a7c4
Guna sekhar <131574717+gunasekhar404@users.noreply.github.com>
2024-02-16 14:18:22 +0530

    Create DataScience.jsx

---
+++ DataScience.jsx
@@ -0,0 +1,164 @@
+import React from "react";
+
+function Datascience() {
+  return (
+    <div className="container">
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/11/Feature-image-Top-High-Paying-Non-Coding-Jobs-in-Data-Science.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Top 10 High Paying Non-Coding Jobs in Data Science in 2024
+              </h5>
+              <p className="card-text">
+                Are you someone who’s not interested in coding, but wants to get
+                placed in tech through data science, and earn a handsome salary?
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">28 November 2023</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/11/Feature-image-1-2048x1024.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Impact of Certification Programs on Hiring Data Scientists
+              </h5>
+              <p className="card-text">
+                Data scientists are the creators behind transforming the raw
+                data into edible data insights. These data insights are
+                supplements for the growth of modern businesses today.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">15 November 2023</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/11/feature-image-product-based-companies-for-data-science-freshers.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Top Product-Based Companies for Data Science Freshers
+              </h5>
+              <p className="card-text">
+                In today’s data-driven world, data science has emerged as a
+                crucial field.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">10 November 2023</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/10/Feature-image-Difference-between-Data-Science-and-Data-Engineering.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                What is the Difference between Data Science and Data
+                Engineering?
+              </h5>
+              <p className="card-text">
+                India has been making some serious waves in the world of data.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">08 November 2023</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/08/data-science-webinars-and-workshops-2048x1072.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Data Science Webinars and Workshops
+              </h5>
+              <p className="card-text">
+                In today’s world, it’s becoming increasingly important to be
+                knowledgeable in the field of data science.With data being such
+                an integral part of nearly every industry, staying up to date on
+                the latest trends and techniques is crucial.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">09 August 2023</small>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/10/Feature-image-Best-books-to-learn-data-science.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Best Data Science Books to Learn in 2024
+              </h5>
+              <p className="card-text">
+                Today, we’re going to talk about something really cool: data
+                science. It’s all about using data to make smart decisions and
+                create amazing things.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">26 October 2023</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Datascience;
