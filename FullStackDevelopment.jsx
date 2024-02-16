e4d4dd55d78ace538d260cb448a7f23d061fa342
Guna sekhar <131574717+gunasekhar404@users.noreply.github.com>
2024-02-16 14:19:38 +0530

    Create FullStackDevelopment.jsx

---
+++ FullStackDevelopment.jsx
@@ -0,0 +1,166 @@
+import React from 'react';
+
+function FullStackDevelopment() {
+  return (
+    <div className="container">
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/11/top-ways-to-assess-soft-skills-in-full-stack-developers.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Top Ways to Assess Soft Skills in Full Stack Developers in 2024
+              </h5>
+              <p className="card-text">
+                When you’re hiring a full-stack developer, what are the most
+                important things you look for? 
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">15 November 2023</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/11/Feature-image-Full-Stack-Developer-vs-Software-Engineer-.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Horizontal vs Vertical Scaling for Efficient System Design
+              </h5>
+              <p className="card-text">
+                In the world of system design, envision a digital skyscraper – a
+                multifaceted structure built to withstand the demands of the
+                modern world.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">15 November 2023</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/11/Feature-image-Horizontal-vs-Vertical-Scaling-for-Efficient-System-Design.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                A Guide to Master UI/UX Design With Online Courses
+              </h5>
+              <p className="card-text">
+                In the evolving world of digitalization, the synthesis of user
+                interface and user experience is the driving force behind
+                digital experiences.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">10 November 2023</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/10/Feature-image-Best-Books-to-Learn-Full-Stack-Development.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Best Books to Learn Full-Stack Development
+              </h5>
+              <p className="card-text">
+                Are you interested in becoming a full-stack developer but not
+                sure where to start? We’ll introduce you to a list
+                of books to learn full-stack development.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">09 November 2023</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/10/Featured-Image-top-product-based-companies-for-full-stack-developers.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Top 10 Product-Based Companies for Full-Stack Developers [2024]
+              </h5>
+              <p className="card-text">
+                In the dynamic landscape of technology, full-stack developers
+                are the architects of the digital world,
+                between front-end and back-end development.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">08 November 2023</small>
+            </div>
+          </div>
+        </div>
+          <div className="col">
+            <div className="card h-100">
+              <img
+                src="https://www.guvi.in/blog/wp-content/uploads/2023/08/Feature-image-Best-Full-Stack-Development-Online-Courses.webp"
+                className="card-img-top"
+                alt=""
+              />
+              <div className="card-body">
+                <h5 className="card-title">
+                  7 Best Full-Stack Development Online Courses [2024]
+                </h5>
+                <p className="card-text">
+                  Today’s world is rapidly evolving into a technological
+                  landscape. Owing to these dynamics, scope for
+                  full-stack developers are constantly on the rise.
+                  <span className="read-more">
+                    <strong>READ MORE</strong>
+                  </span>
+                </p>
+              </div>
+              <div className="card-footer">
+                <small className="text-body-secondary">31 August 2023</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  
+  );
+}
+
+export default FullStackDevelopment;
