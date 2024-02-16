5fa56ec1bcd3bbd240f8cbafa59d8ba7ec89438b
Guna sekhar <131574717+gunasekhar404@users.noreply.github.com>
2024-02-16 14:16:22 +0530

    Create Career.jsx

---
+++ Career.jsx
@@ -0,0 +1,168 @@
+import React from "react";
+
+function Career() {
+  return (
+    <div className="container">
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/11/Feature-image-Mechanical-Engineering-Resume-Important-Things-To-Look-Out-For.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Mechanical Engineering Resume: 9 Important Things To Look Out
+                For
+              </h5>
+              <p className="card-text">
+                With the development of technology, most of us started
+                forgetting about mechanical engineering which pioneered the
+                whole industrial revolution.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">13 December 2023</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/12/Feature-image-Top-IT-Jobs-for-Economics-Students.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Top 5 IT Jobs for Economics Students
+              </h5>
+              <p className="card-text">
+                In today’s digital age, the intersection of economics and
+                technology offers exciting career opportunities for students
+                with a background in economics.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">04 December 2023</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/12/Feature-image-Top-IT-Jobs-for-Commerce-Students.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Top IT Jobs for Commerce Students: A Lucrative Career Path
+              </h5>
+              <p className="card-text">
+                With the rapid advancement of technology, the demand for IT
+                professionals has soared in recent years.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">02 December 2022</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/12/Feature-image-Automation-Test-Engineer-Resume-Important-Things-To-Consider.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Automation Test Engineer Resume: 10 Important Things To Consider
+              </h5>
+              <p className="card-text">
+                The world is moving towards automating the testing of products
+                in order to increase work efficiency.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">01 December 2023</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/11/FEATURE-IMAGE-1-3-2048x1072.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Professional Civil Engineer Resume: A Guide To Attract Employers
+                in 2024
+              </h5>
+              <p className="card-text">
+                The world is moving towards modernization leading to an increase
+                in the popularity of civil engineering. The field is getting
+                competitive and it is important for you to stay ahead of the
+                crowd to achieve big.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">01 December 2023</small>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/11/Featured-Image-2048x1072.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                9 Best Product-Based Companies for Project Management
+              </h5>
+              <p className="card-text">
+                In today’s tech-driven world, the demand for project managers is
+                higher than before. The workload of every other company is
+                exponential and they need project managers to maintain them
+                effectively.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">01 December 2023</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Career;
