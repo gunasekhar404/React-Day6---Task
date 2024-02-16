08f8f60356ddaea0cec26201e7e0d0f0246a93ef
Guna sekhar <131574717+gunasekhar404@users.noreply.github.com>
2024-02-16 14:17:29 +0530

    Create CyberSecurity.jsx

---
+++ CyberSecurity.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+
+function CyberSecurity() {
+  return (
+    <div className="container">
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/12/Feature-image-Non-Coding-Jobs-in-Cybersecurity.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Non-Coding Jobs in Cybersecurity: A Comprehensive Guide
+              </h5>
+              <p className="card-text">
+                In the rapidly evolving field of cybersecurity, there is a
+                common misconception that coding skills.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">04 December 2023</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2023/09/Feature-image-What-Is-Hacking_-Types-of-Hacking-More.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                What Is Hacking? Types of Hacking & More
+              </h5>
+              <p className="card-text">
+                Have you ever wondered what hacking is all about? It’s a big
+                deal in today’s tech-heavy world.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">25 September 2023</small>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="https://www.guvi.in/blog/wp-content/uploads/2021/03/The-Ultimate-%E2%80%A8Cybersecurity-Roadmap-for-Beginners.webp"
+              className="card-img-top"
+              alt=""
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                The Ultimate Cybersecurity Roadmap for Beginners
+              </h5>
+              <p className="card-text">
+                Cybersecurity jobs are also one of the most handsomely paying
+                jobs in recent times. Furthermore, the annual growth rate of
+                cybersecurity jobs as identified by LinkedIn is 30%.
+                <span className="read-more">
+                  <strong>READ MORE</strong>
+                </span>
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary">01 March 2021</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CyberSecurity;
